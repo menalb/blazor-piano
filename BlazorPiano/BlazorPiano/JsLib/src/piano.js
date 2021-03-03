fc260d2b63cded6076833d9c9f413bf8d2caef75
Alberto Meneghini <menalb84@gmail.com>
2021-03-03 21:47:38 +0100

    piano

--- BlazorPiano/BlazorPiano/JsLib/src/piano.js
+++ BlazorPiano/BlazorPiano/JsLib/src/piano.js
@@ -1,6 +1,15 @@
 import * as Tone from 'tone'
 
-const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Sampler({
+    urls: {
+        "C4": "C4.mp3",
+        "D#4": "Ds4.mp3",
+        "F#4": "Fs4.mp3",
+        "A4": "A4.mp3",
+    },
+    release: 1,
+    baseUrl: "https://tonejs.github.io/audio/salamander/",
+}).toDestination();
 
 export function play(note) {
     synth.triggerAttackRelease(note, "8n");
