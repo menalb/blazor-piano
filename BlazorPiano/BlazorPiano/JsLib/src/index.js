41cfeb75c7e8de483af36ad2d8ac0158367c0841
Alberto Meneghini <menalb84@gmail.com>
2021-03-03 21:11:38 +0100

    prees and release

--- BlazorPiano/BlazorPiano/JsLib/src/index.js
+++ BlazorPiano/BlazorPiano/JsLib/src/index.js
@@ -1,5 +1,15 @@
-import { play } from './piano';
+import { keyAttack, keyRelease, play } from './piano';
 
 export function playNote(note) {
     play(note);
+}
+
+export function attack(note) {
+    keyAttack(note);
+    console.log("attack " + note);
+}
+
+export function release(note) {
+    keyRelease();
+    console.log("release " + note);
 }
--- BlazorPiano/BlazorPiano/JsLib/src/piano.js
+++ BlazorPiano/BlazorPiano/JsLib/src/piano.js
@@ -1,7 +1,17 @@
 import * as Tone from 'tone'
 
-export function play(note) {
-    const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Synth().toDestination();
 
+export function play(note) {
     synth.triggerAttackRelease(note, "8n");
+}
+
+
+export function keyAttack(note) {
+    synth.triggerAttack(note);
+}
+
+
+export function keyRelease() {
+    synth.triggerRelease(Tone.now());
 }
