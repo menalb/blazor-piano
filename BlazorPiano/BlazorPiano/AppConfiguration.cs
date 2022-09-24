e090bacc411f1829ccd4329dbb6622d7682437f4
Alberto Meneghini <menalb84@gmail.com>
2022-09-24 19:11:49 +0200

    refactoring configuration

---
+++ BlazorPiano/BlazorPiano/AppConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace BlazorPiano
+{
+    public class OnlineConfiguration
+    {
+        public OnlineConfiguration()
+        {
+
+        }
+
+        public const string ConfigurationSettingName = "OnlineConfiguration";
+        public bool IsOnline =>
+            WebSocketConfiguration is not null && !string.IsNullOrEmpty(WebSocketConfiguration.Uri)
+            &&
+            BlazorPianoApi is not null && !string.IsNullOrEmpty(BlazorPianoApi.Uri);
+
+        public WebSocketApiConfigOption WebSocketConfiguration { get; set; } = new WebSocketApiConfigOption();
+        public BlazorPianoApiConfiguration BlazorPianoApi { get; set; } = new BlazorPianoApiConfiguration();
+    }
+
+    public class BlazorPianoApiConfiguration
+    {
+        public string Uri { get; set; } = "";
+    }
+
+    public class WebSocketApiConfigOption
+    {
+        public string Uri { get; set; } = "";
+    }
+}
--- BlazorPiano/BlazorPiano/Program.cs
+++ BlazorPiano/BlazorPiano/Program.cs
@@ -3,7 +3,9 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using BlazorPiano.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace BlazorPiano
 {
@@ -13,24 +15,30 @@ namespace BlazorPiano
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-            
-            builder.Services.AddScoped(sp => 
-            new HttpClient 
-            { 
-                BaseAddress = new Uri(builder.Configuration["BlazorPianoApi:Uri"] ?? throw new ArgumentNullException("Invalid BlazorPiano API Uri in configuration")) 
-            });
 
             builder.Services.AddScoped<PianoGateway>();
-            builder.Services.AddScoped<ConnectedPlayersService>();
             builder.Services.AddSingleton<PianoService>();
             builder.Services.AddSingleton<PlayerService>();
-            builder.Services.AddSingleton<BrowserResizeService>();            
+            builder.Services.AddScoped<ConnectedPlayersService>();
+            builder.Services.AddSingleton<BrowserResizeService>();
+            
+            builder.Services
+                .AddOptions<OnlineConfiguration>()
+                .Bind(builder.Configuration.GetSection("OnlineConfiguration"));
 
-            builder.Services.AddSingleton(
-                new WebSocketApiConfig 
-                { 
-                    Uri = builder.Configuration["WebSocketApi:Uri"] ?? throw new ArgumentNullException("Invalid Websocket API Uri in configuration") 
-                });
+            builder.Services.AddScoped(sp =>
+            {
+                var config = sp.GetService<IOptions<OnlineConfiguration>>();
+
+                if (config is null) throw new ArgumentNullException(nameof(OnlineConfiguration));
+
+                return config.Value.IsOnline ?
+                new HttpClient
+                {
+                    BaseAddress = new Uri(config.Value.BlazorPianoApi.Uri)
+                }
+                : new HttpClient();
+            });
 
             await builder.Build().RunAsync();
         }
