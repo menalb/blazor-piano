ec25ca4084b20241133071d4bb2084ffb25fbba2
Alberto Meneghini <menalb84@gmail.com>
2022-09-30 09:39:12 +0200

    refactoring notifications

--- BlazorPiano/BlazorPiano/Services/PianoService.cs
+++ BlazorPiano/BlazorPiano/Services/PianoService.cs
@@ -6,19 +6,24 @@ namespace BlazorPiano.Services;
 
 public class PianoService
 {
-    public event Func<Note,Task> OnAttackNote;
-    public event Func<Note, Task> OnReleaseNote;
+    public event Func<Note,Task> OnAttackNoteAsync;
+    public event Action<Note> OnAttackNote;
+
+    public event Func<Note, Task> OnReleaseNoteAsync;
+    public event Action<Note> OnReleaseNote;
 
     public event Func<Note,Task> OnToAttackNote;
     public event Func<Note,Task> OnToReleaseNote;
 
     public void NotifyAttackedNote(Note note)
     {        
+        OnAttackNoteAsync?.Invoke(note);
         OnAttackNote?.Invoke(note);
     }
 
     public void NotifyReleasedNote(Note note)
     {
+        OnReleaseNoteAsync?.Invoke(note);
         OnReleaseNote?.Invoke(note);
     }
 
